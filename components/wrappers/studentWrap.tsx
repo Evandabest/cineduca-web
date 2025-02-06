4e8e9d88dbf0e4f15b764f8cb3d7c97e06c07ce9
Evandabest <evanhaque1@gmail.com>
2025-02-06 15:40:37 -0500

    fix: conformed to eslint

--- components/wrappers/studentWrap.tsx
+++ components/wrappers/studentWrap.tsx
@@ -6,9 +6,10 @@ import { redirect } from 'next/navigation'
 
 
 async function studentWrap ({children, role }: Readonly<{children: React.ReactNode, role: string}>) {
-    if (!("Student_" in (children?.toString))) {
+    if (!children || !children.toString().includes("Student_")) {
         redirect("/")
     }
+    console.log(role)
     return (
     <div>
         {children}
