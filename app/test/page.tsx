237ff68818c0921ab278701e2d8808f70ce19c8f
Evandabest <evanhaque1@gmail.com>
2025-02-20 16:23:18 -0500

    feat: place holder pages for different functions

---
+++ app/test/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useState } from "react"
+
+
+const test = () => {
+
+    const [number, setNumber] = useState(0)
+
+    //fizzbuzz
+    
+    return (
+        <>
+        </>
+    )
+}
+
+export default test
